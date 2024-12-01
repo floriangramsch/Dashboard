7f21f0e373a553f8dd7624872ed18dd1ca5fff37
Florian Gramsch <florigram@gmail.com>
2024-12-01 22:26:40 +0100

    remove advent.. you know why

--- script.js
+++ script.js
@@ -30,11 +30,6 @@ const links = [
     favicon: "/android-chrome-512x512.png",
   },
   {
-    name: "Advent",
-    base: "https://advent.floxsite.de:2604",
-    favicon: "/web-app-manifest-512x512.png",
-  },
-  {
     name: "Fiona",
     base: "https://fiona.floxsite.de:2604",
     favicon: "/web-app-manifest-512x512.png",
